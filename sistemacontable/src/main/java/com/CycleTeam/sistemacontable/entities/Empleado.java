c1fd6da9e68176683f044e07125a0ce1237ad61f
MarcoAgredo <marcoagredo@unicauca.edu.co>
2022-09-02 20:13:46 -0500

    Ajustes a la clase Empleado, creada por Alfonso

--- sistemacontable/src/main/java/com/CycleTeam/sistemacontable/entities/Empleado.java
+++ sistemacontable/src/main/java/com/CycleTeam/sistemacontable/entities/Empleado.java
@@ -1,12 +1,16 @@
 // Clase empleado
-
-
 package com.CycleTeam.sistemacontable.entities;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.*;
 
 @NoArgsConstructor
 @Getter
 @Setter
-
 @Entity
 @Table(name = "empleados")
 public class Empleado {
@@ -14,25 +18,21 @@ public class Empleado {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   private int id;
-
-  private String nombre
+  private String nombre;
   @ManyToOne
   @JoinColumn(name = "empresa_pertenece_id")
   private Empresa empresaPertenece;
-  private String correo:
+  private String correo;
   private String rolEmpleado;
-  
+
   //Constructor 
-  
-  public Empleado(String nombre, String correo, String empresaPertenece, String rolEmpleado) {
+  public Empleado(String nombre, String correo, Empresa empresaPertenece, String rolEmpleado) {
         this.nombre = nombre;
         this.correo = correo;
         this.empresaPertenece = empresaPertenece;
         this.rolEmpleado = rolEmpleado;
-    }
-
+  }
 }
-
 
 // By: 3CycleTeam
 

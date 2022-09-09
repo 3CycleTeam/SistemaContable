5811af25c23faa159a664d38055cca1cde03e2aa
AlfonsoDC <aduranc@correo.udistrital.edu.co>
2022-09-08 21:42:34 -0500

    Borrar restController Movimiento de Dinero

--- sistemacontable/src/main/java/com/CycleTeam/sistemacontable/controllers/Controllers.java
+++ sistemacontable/src/main/java/com/CycleTeam/sistemacontable/controllers/Controllers.java
@@ -2,8 +2,10 @@ package com.CycleTeam.sistemacontable.controllers;
 
 import com.CycleTeam.sistemacontable.entities.Empleado;
 import com.CycleTeam.sistemacontable.entities.Empresa;
+import com.CycleTeam.sistemacontable.entities.MovimientoDinero;
 import com.CycleTeam.sistemacontable.services.EmpleadoService;
 import com.CycleTeam.sistemacontable.services.EmpresaServicios;
+import com.CycleTeam.sistemacontable.services.MoviDinerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,6 +19,9 @@ public class Controllers{
 
     @Autowired
     EmpleadoService empleadoService;
+
+    @Autowired
+    MoviDinerService moviDinerService;
 
 
     @GetMapping ("/Empresas")
@@ -99,8 +104,36 @@ public class Controllers{
         else {
             return "No se pudo eliminar el empleado con el Id"+id;
         }
-
     }
+
+    //Movimiento de dinero
+
+        @GetMapping("/Movimientos")//Consultar todos los movimientos
+        public List<MovimientoDinero> listarMovimientoDinero(){
+            return this.moviDinerService.getAllMovimientoDinero();
+        }
+
+        @PostMapping ("/Movimiento")//Guardar todos los movimientos
+        public MovimientoDinero crearMovimientoDinero(@RequestBody MovimientoDinero nuevoMovimientoDinero){
+            return this.moviDinerService.crearMovimientoDinero(nuevoMovimientoDinero);
+        }
+
+        @GetMapping("/Movimientos/{id}") ////Consultar movimiento por id
+        public MovimientoDinero buscarMoviDinerbyId(@PathVariable("id") Integer id){
+            return moviDinerService.buscarMoviDinerbyId(id);
+
+        }
+
+
+    ///  @PatchMapping("/movimientos/{id}")//Editar o actualizar un movimiento
+    ///  public MovimientoDinero actualizarMovimientoDinero(@PathVariable("id")Integer id, @RequestBody MovimientoDinero MovimientoDineroActualizar){
+    ///     MovimientoDinero mov=MoviDinerService.guardarActualizarMovimiento(id);
+    ///     mov.setConceptoMovimiento(mov.getConceptoMovimiento());
+    ///    mov.setMovimientoDinero(mov.getMovimientoDinero());
+    ///    mov.setEmpleadoMovimiento  (mov.getEmpleadoMovimiento());
+
+
+    /// }
 }
 
 

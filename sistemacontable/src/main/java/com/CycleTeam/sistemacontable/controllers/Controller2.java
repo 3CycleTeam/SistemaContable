d4088f66941686eae978fb3e8a911852a8d5b9fa
juanguma <julianguapacha@hotmail.com>
2022-09-14 16:36:14 -0500

    Ultimos Ajustes JG

--- sistemacontable/src/main/java/com/CycleTeam/sistemacontable/controllers/Controller2.java
+++ sistemacontable/src/main/java/com/CycleTeam/sistemacontable/controllers/Controller2.java
@@ -2,10 +2,14 @@ package com.CycleTeam.sistemacontable.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
 @Controller
 public class Controller2 {
     @GetMapping("/julian")
-    public String julian (){
+    public String julian() {
         return "indexJulian";
     }
-}
+
+
+
+        }

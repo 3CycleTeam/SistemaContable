ea86106d750fa9dee88483493cb968b3b916de78
juanguma <julianguapacha@hotmail.com>
2022-09-15 08:50:43 -0500

    Se crea Template para mostrar empresas.

--- sistemacontable/src/main/java/com/CycleTeam/sistemacontable/controllers/Controller2.java
+++ sistemacontable/src/main/java/com/CycleTeam/sistemacontable/controllers/Controller2.java
@@ -1,15 +1,27 @@
 package com.CycleTeam.sistemacontable.controllers;
 
+import com.CycleTeam.sistemacontable.entities.Empresa;
+import com.CycleTeam.sistemacontable.services.EmpresaServicios;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class Controller2 {
-    @GetMapping("/julian")
-    public String julian() {
-        return "indexJulian";
-    }
+
+    @Autowired
+    EmpresaServicios empresaServicios;
+    @GetMapping("/mostrarempresas")
+    public String mostrarempresas( Model model ) {
+        List<Empresa> listaempresas = this.empresaServicios.getAllEmpresas();
+        model.addAttribute("listaempresas",listaempresas);
+        return "showenterprises";   }
 
 
 
         }
+
+

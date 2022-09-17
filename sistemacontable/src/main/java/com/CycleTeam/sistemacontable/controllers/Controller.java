870904d4f8e78b8908fe6c24ab14d5504de2a893
Mareti1986 <mareti18@gmail.com>
2022-09-16 21:03:07 -0500

    Se ajusta plantilla con los colores

--- sistemacontable/src/main/java/com/CycleTeam/sistemacontable/controllers/Controller.java
+++ sistemacontable/src/main/java/com/CycleTeam/sistemacontable/controllers/Controller.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
@@ -67,8 +69,6 @@ public class Controller {
         this.empresaServicios.guardarOActualizaEmpresa(emp);
         return "redirect:/showenterprises";
     }
-
-
 
 
 }

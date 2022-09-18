c985deb21237cc16454d5258a1cc402e5251612f
AlfonsoDC <aduranc@correo.udistrital.edu.co>
2022-09-18 16:42:48 -0500

    Configuración index

--- sistemacontable/src/main/java/com/CycleTeam/sistemacontable/controllers/Controller.java
+++ sistemacontable/src/main/java/com/CycleTeam/sistemacontable/controllers/Controller.java
@@ -7,6 +7,7 @@ import com.CycleTeam.sistemacontable.services.EmpleadoService;
 import com.CycleTeam.sistemacontable.services.EmpresaServicios;
 import com.CycleTeam.sistemacontable.services.PerfilService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -28,6 +29,7 @@ public class Controller {
     EmpleadoService empleadoService;
     @Autowired
     PerfilService perfilService;
+
 
     @GetMapping("/empresas/mostrar")
 
